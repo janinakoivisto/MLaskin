100cc197ab224290b45e4d7d3a5c108d9b353167
Janina Koivisto <koivistokms@gmail.com>
2023-08-28 15:31:46 +0300

    Tehty tehtävä

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, Button} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [result,
+        setResult] = useState('');
+
+    const handleAdd = () => {
+        const sum = parseInt(num1) + parseInt(num2);
+        setResult(`Result: ${sum.toString()}`);    };
+
+    return (
+        <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Enter the first number"
+          value={num1}
+          onChangeText={text => setNum1(text)}
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Enter the second number"
+          value={num2}
+          onChangeText={text => setNum2(text)}
+        />
+        <Button title="Add" onPress={handleAdd} />
+        <Text style={styles.result}>{result}</Text>
+      </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    input: {
+      width: '80%',
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+    result: {
+      marginTop: 20,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  });
+  
